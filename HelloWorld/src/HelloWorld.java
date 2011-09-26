f09ef0732d1f1cab37e2e50e5b66df71ea27db30
Tony Lin <nintendolintao@gmail.com>
2011-09-26 15:23:32 +0800

    Use HelloPrinter
    Signed-off-by: Tony Lin <nintendolintao@gmail.com>

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,8 +6,10 @@ public class HelloWorld {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello Tony!");
+//        System.out.println("Hello World!");
+//        System.out.println("Hello Tony!");
+
+        HelloPrinter.printHelloWorld();
     }
 
 }
