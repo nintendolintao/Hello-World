b120290c8527d36e7e9d2c0821e1294fd8bd3a04
Tony Lin <nintendolintao@gmail.com>
2011-09-26 16:25:27 +0800

    print oscar

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -10,14 +10,7 @@ public class HelloWorld {
         HelloPrinter.printHelloWorld();
         HelloPrinter.printHelloTony();
         HelloPrinter.printHelloTim();
-
-        System.out.println("Hello Tony 123 !");
-        System.out.println("Hello Tony 456 !");
-        System.out.println("Hello Tony 789 !");
-
-        System.out.println("Hello Tony 22 !");
-        System.out.println("Hello Tony 22 !");
-        System.out.println("Hello Tony 22 !");
+        HelloPrinter.printHelloOscar();
     }
 
 }
