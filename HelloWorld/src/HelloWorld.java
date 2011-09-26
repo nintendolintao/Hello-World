16700d6f63dc5f687641815cf37748644fe9a2a4
Tony Lin <nintendolintao@gmail.com>
2011-09-26 16:17:48 +0800

    uncomment prints

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -7,11 +7,13 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
 
-//        HelloPrinter.printHelloWorld();
-//        HelloPrinter.printHelloTony();
-//        HelloPrinter.printHelloTim();
-//
-//        System.out.println("Hello Tony 789 !");
+        HelloPrinter.printHelloWorld();
+        HelloPrinter.printHelloTony();
+        HelloPrinter.printHelloTim();
+
+        System.out.println("Hello Tony 123 !");
+        System.out.println("Hello Tony 456 !");
+        System.out.println("Hello Tony 789 !");
     }
 
 }
