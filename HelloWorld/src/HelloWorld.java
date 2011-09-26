573a7c625d197dc6e57257ff8c62c12bb625b030
Tony Lin <nintendolintao@gmail.com>
2011-09-26 16:14:11 +0800

    tony ???

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -8,9 +8,10 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         HelloPrinter.printHelloWorld();
+        HelloPrinter.printHelloTony();
+        HelloPrinter.printHelloTim();
 
-        System.out.println("Hello Tony 123 !");
-        System.out.println("Hello Tony 456 !");
+        System.out.println("Hello Tony 789 !");
     }
 
 }
